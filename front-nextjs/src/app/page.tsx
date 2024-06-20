7fc4d75beca61342259ec58a2a7cea3c54499bfc
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-06-20 12:58:28 -0300

    feat: front-next: add server side fetch data in home page

--- front-nextjs/src/app/page.tsx
+++ front-nextjs/src/app/page.tsx
@@ -1,10 +1,19 @@
 import { EventCard } from '@/components/EventCard';
 import { Title } from '@/components/Title';
 import { EventModel } from '@/models/event.model';
-import { listEvents } from '@/models/mocks/list-events';
 
-export default function HomePage() {
-  const events: EventModel[] = listEvents();
+export async function getEvents(): Promise<{ events: EventModel[] }> {
+  const response = await fetch('http://localhost:8080/events', {
+    cache: 'no-store',
+    // next: {
+    //   revalidate: 10, // 10 seconds
+    // },
+  });
+  return response.json();
+}
+
+export default async function HomePage() {
+  const { events } = await getEvents();
 
   return (
     <main className="mt-10 flex flex-col">
